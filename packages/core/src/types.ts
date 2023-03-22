b4b1b9ad14c93533e2451fb6439d3182fc7bbceb
Javi Velasco <javier.velasco86@gmail.com>
2023-03-22 14:02:36 +0100

    Remove `Dictionary` type

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -13,11 +13,3 @@ export interface RejectionHandler {
 export interface ErrorHandler {
   (error?: {} | null): void
 }
-
-/**
- * Just any type of object indexed by string or numbers where the value can
- * be anything or the provided generic.
- */
-export interface Dictionary<T = any> {
-  [key: string | number]: T
-}
